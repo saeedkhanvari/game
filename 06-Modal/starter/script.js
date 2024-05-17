79c0b6958b81a150016d80c9b182af889f0be7c1
CallMeSaeed <86480508+saeedkhanvari@users.noreply.github.com>
2024-05-17 17:52:13 +0330

    Building Modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+function openModal(handler) {
+  handler.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+function closeModal(handler) {
+  handler.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+}
+
+btnOpenModal.forEach(button => {
+  openModal(button)
+});
+ 
+closeModal(btnCloseModal);
+closeModal(overlay);
+
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'Escape'){
+        if(!modal.classList.contains('hidden')){
+            modal.classList.add('hidden');
+            overlay.classList.add('hidden');
+        }
+    }
+})
